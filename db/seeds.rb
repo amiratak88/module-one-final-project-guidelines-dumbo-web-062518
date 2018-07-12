1fb0080ae4f79ad664738c17d754f6e852ad1a23
Dan Chung <danetix@gmail.com>
2018-07-12 12:19:00 -0400

    seeding for weather_types and weather_pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+##
+## METHODS
+##
+
 def populate_pokemon
   counter = 1
   until counter == 15 do
@@ -10,11 +14,98 @@ def populate_pokemon
   end
 end
 
-populate_pokemon
+def populate_weather_types
+  ## cloud, clear, thunderstorm, snow, rain
+  WeatherType.create(name: 'cloud')
+  WeatherType.create(name: 'clear')
+  WeatherType.create(name: 'thunderstorm')
+  WeatherType.create(name: 'snow')
+  WeatherType.create(name: 'rain')
+end
 
-## cloud, clear, thunderstorm, snow, rain
-WeatherType.create(name: 'cloud')
-WeatherType.create(name: 'clear')
-WeatherType.create(name: 'thunderstorm')
-WeatherType.create(name: 'snow')
-WeatherType.create(name: 'rain')
+def populate_weather_pokemon
+  pokemons = Pokemon.all
+  pokemons.each do |pokemon|
+
+    #type_1
+    weather_type = get_pokemon_weather_type_id(pokemon.type_1)
+    weather = WeatherType.find_by(name: weather_type)
+    WeatherPokemon.create(weather_type_id: weather.id, pokemon_id: pokemon.id)
+
+    #type_2
+    if !pokemon.type_2.nil?
+      weather_type = get_pokemon_weather_type_id(pokemon.type_2)
+      weather = WeatherType.find_by(name: weather_type)
+      WeatherPokemon.create(weather_type_id: weather.id, pokemon_id: pokemon.id)
+    end
+  end
+end
+
+def get_pokemon_weather_type_id(pokemon_type)
+  ## cloud, clear, thunderstorm, snow, rain
+  weather_type = String.new
+
+  case pokemon_type.downcase
+  when 'normal'
+    weather_type = 'clear'
+  when 'fighting'
+    weather_type = 'clear'
+  when 'flying'
+    weather_type = 'clear'
+  when 'poison'
+    weather_type = 'cloud'
+  when 'ground'
+    weather_type = 'cloud'
+  when 'rock'
+    weather_type = 'cloud'
+  when 'bug'
+    weather_type = 'clear'
+  when 'ghost'
+    weather_type = 'cloud'
+  when 'steel'
+    weather_type = 'clear'
+  when 'fire'
+    weather_type = 'clear'
+  when 'water'
+    weather_type = 'rain'
+  when 'grass'
+    weather_type = 'clear'
+  when 'electric'
+    weather_type = 'thunderstorm'
+  when 'psychic'
+    weather_type = 'clear'
+  when 'ice'
+    weather_type = 'snow'
+  when 'dragon'
+    weather_type = 'clear'
+  when 'dark'
+    weather_type = 'cloud'
+  when 'fairy'
+    weather_type = 'clear'
+  else
+    weather_type = 'clear'
+  end
+
+  weather_type.downcase
+end
+
+##
+## RUN METHODS
+##
+
+clear_screen
+
+spinner = TTY::Spinner.new("[:spinner] Populating pokemon...", format: :spin_2)
+spinner.auto_spin
+populate_pokemon
+sleep(0.3)
+
+spinner = TTY::Spinner.new("[:spinner] Populating weather types...", format: :spin_2)
+spinner.auto_spin
+populate_weather_types
+sleep(0.3)
+
+spinner = TTY::Spinner.new("[:spinner] Populating weather_pokemon...", format: :spin_2)
+spinner.auto_spin
+populate_weather_pokemon
+sleep(0.3)
