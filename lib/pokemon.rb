dbeb7f62df9c76290a437a5c488870deaf06f869
github username <github email address>
2018-07-12 12:00:54 -0400

    tinkering, nothing should be different

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -78,9 +78,9 @@ class Pokemon < ActiveRecord::Base
 
   def self.generate_pokemon
   #generates random pokemon from array
-    found_pokemon = self.find_by(pokedex_id: rand(1..15))
+    found_pokemon = self.find_by(pokedex_id: rand(1...15))
     p "A wild #{found_pokemon.name} has appeared!"
-    found_pokemon.display_image
+    found_pokemon.display_location
     found_pokemon
     # catch_pokemon(found_pokemon)
   # Encounter.all
