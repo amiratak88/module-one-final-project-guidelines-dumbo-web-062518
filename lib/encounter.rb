f4c1dc01ec6f9762e0c40647bb332c34e9a76d08
github username <github email address>
2018-07-13 09:01:25 -0400

    minor syntax change

--- lib/encounter.rb
+++ lib/encounter.rb
@@ -3,6 +3,6 @@ class Encounter < ActiveRecord::Base
   belongs_to :pokemon
 end
 
-def pokemon_by_location
-  Visit.find(self.visit_id)
+def pokemon_locale
+  Location.find(Visit.find(self.visit_id).location_id)
 end
