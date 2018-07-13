e535eb10e5ccd9dd9c73a281783d794c2c263abc
github username <github email address>
2018-07-13 10:34:58 -0400

    added pokemon status if it pops out

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -136,6 +136,7 @@ class Trainer < ActiveRecord::Base
       spinner.stop("#{found_pokemon.name} popped out!".yellow)
       sleep(1.5)
       clear_screen
+      pokemon_status(found_pokemon, pokemon_hp)
       pokemon_runaway(found_pokemon)
       found_pokemon.display_image
       battle_menu(found_pokemon, pokemon_hp, self)
